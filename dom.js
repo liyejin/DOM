c93120b8f3f30ff0fbfac00edf5ae07e16d4d5bf
Jin <yesjintonic@gmail.com>
2023-05-11 20:20:18 +0900

    엘리먼트 객체 선택하기, 선택하는 일반적인 방법

--- dom.js
+++ dom.js
@@ -1,16 +1,31 @@
-//[#page load event]
-function printResult() {
-  var x = prompt("x의 값을 입력하세요");
-  var y = prompt("y의 값을 입력하세요");
-
-  x = parseInt(x);
-  y = parseInt(y);
-
-  console.log(x + y);
-}
+//<h1>1. 노드 선택방법</h1>------------------------------------------
 
 window.addEventListener("load", function () {
+  var section = document.getElementById("s1");
+  var input = section.getElementsByClassName("input")[0];
+  var button = section.getElementsByClassName("button")[0];
+
+  button.onclick = function () {
+    input.value = "안녕하세요";
+  };
+});
+
+//------------------------------------------------------------------
+
+window.addEventListener("load", function () {
+  var btnPrint = this.document.getElementById("btn-print");
   btnPrint.onclick = printResult;
+
+  //[#page load event], 함수 지역화(고립)
+  function printResult() {
+    var x = prompt("x의 값을 입력하세요");
+    var y = prompt("y의 값을 입력하세요");
+
+    x = parseInt(x);
+    y = parseInt(y);
+
+    console.log(x + y);
+  }
 });
 
 // window.onload = function () {
