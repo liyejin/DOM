57cd42a3b660bb7976212e8e744c7351b458d8a3
Jin <yesjintonic@gmail.com>
2023-05-21 17:08:24 +0900

    스타일 다루기

--- dom.js
+++ dom.js
@@ -1,4 +1,37 @@
-//<h1>3. 이벤트 객체 : 개선된 계산기</h1>------------------------------------------
+//4. 스타일 다루기 : 값 입력과 동적으로 박스 스타일 변경------------------------------------------
+window.addEventListener("load", function () {
+  var section = document.querySelector("#s4");
+  var styleInput = section.querySelector(".style-input");
+  var widthInput = section.querySelector(".width-input");
+  var radiusInput = section.querySelector(".radius-input");
+  var colorInput = section.querySelector(".color-input");
+
+  var item = section.querySelector(".item");
+  var outputDiv = section.querySelector(".output");
+
+  // oninput => 최종적으로 value의 값이 바뀌었을 때
+  styleInput.oninput = function (e) {
+    item.style.borderStyle = styleInput.value;
+    outputDiv.innerText = item.style.cssText;
+  };
+
+  widthInput.oninput = function (e) {
+    item.style.borderWidth = widthInput.value + "px";
+    outputDiv.innerText = item.style.cssText;
+  };
+
+  radiusInput.oninput = function (e) {
+    item.style.borderRadius = radiusInput.value + "px";
+    outputDiv.innerText = item.style.cssText;
+  };
+
+  colorInput.oninput = function (e) {
+    item.style.borderColor = colorInput.value;
+    outputDiv.innerText = item.style.cssText;
+  };
+});
+
+//3. 이벤트 객체 : 개선된 계산기------------------------------------------
 window.addEventListener("load", function () {
   var section = document.querySelector("#s3");
   var txtInput = section.querySelector("input[type=text]");
@@ -10,7 +43,7 @@ window.addEventListener("load", function () {
   };
 });
 
-//<h1>2. DOM 속성 다루기 : 계산기</h1>------------------------------------------
+//2. DOM 속성 다루기 : 계산기------------------------------------------
 
 window.addEventListener("load", function () {
   var section = document.querySelector("#s2");
@@ -27,7 +60,7 @@ window.addEventListener("load", function () {
   };
 });
 
-//<h1>1. 노드 선택방법</h1>------------------------------------------
+//1. 노드 선택방법------------------------------------------
 
 window.addEventListener("load", function () {
   // [#dom이 갖고있는 방법]
